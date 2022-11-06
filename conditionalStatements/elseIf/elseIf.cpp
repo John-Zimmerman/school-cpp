9a6668470133163813d29cab0a9d4dcd95dd8fc8
John Zimmerman <jzimmerman71991@gmail.com>
2022-11-06 13:10:06 -0600

    Added directory Conditionalstatements

---
+++ conditionalStatements/elseIf/elseIf.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i = 10;
+    int n = 5;
+
+    if (i < n) {
+        std:cout << "false" << std::endl;
+    }
+    else if (i == n) {
+        std::cout << "false" << std::endl;
+    }
+    else {
+        std::cout << "true" << std::endl;
+    }
+    return 0;
+}
